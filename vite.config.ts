6fa38d57f73a09e1c50cf84d1b0b5d3b3c0915be
thanhtunguet <thanhtung.uet@gmail.com>
2025-11-28 09:11:47 +0700

    feat: add GitHub Actions workflow for build and deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ffmpegjs-demo/',
   plugins: [react()],
   server: {
     headers: {
